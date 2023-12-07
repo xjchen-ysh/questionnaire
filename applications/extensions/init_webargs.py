1245dc541f463172fabd1064a121e68d4d6de46e
不胜舟 <1650473152@qq.com>
2023-12-07 14:00:29 +0800

    增加flask-wtf，引入csrf token验证

--- applications/extensions/init_webargs.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import EXCLUDE
-from webargs.flaskparser import FlaskParser
-
-
-class Parser(FlaskParser):
-    DEFAULT_UNKNOWN_BY_LOCATION = {"query": EXCLUDE}
-
-
-parser = Parser()
